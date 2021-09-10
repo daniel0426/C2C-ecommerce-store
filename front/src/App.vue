625823d0e49ebe4d42753ad1705f167fb1631ce2
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-09-10 22:04:15 +1200

    Added brand colours and font and styled mobile header

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <NavBar/>
+
+    <router-view/>
+
   </div>
-  <router-view/>
+
 </template>
 
+<script>
+
+import NavBar from './components/NavBar.vue'
+
+export default {
+  components:{
+    NavBar
+  }
+  
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+@import url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap');
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+
+    extend: {
+
+      fontFamily:{
+        'epilogue': ['epilogue', 'sans-serif']
+      },
+
+      colors:{
+        'bright-purple': '#ba82ee',
+        'dark-purple': '#534ae0',
+        'purple-grey': '#8585b0',
+        'light-purple': '#a695b4',
+        'cream': '#f7f7f6'
+      },
+    },
   },
   variants: {
     extend: {},
