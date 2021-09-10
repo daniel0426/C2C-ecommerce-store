5bc45df2d5cecddf37ce0eea72c28ccb224b1e23
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-09-10 17:56:30 +1200

    Scaffold out vue router for page access from Home

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    
+    <div class="flex flex-col">
+      <router-link to="/CreateAccount">Create Account </router-link>
+      <router-link to="/CreateListing"> Create Listing </router-link>
+      <router-link to="/Detail"> Detail </router-link>
+      <router-link to="/Login"> Login </router-link>
+      <router-link to="/MyListings"> My Listings </router-link>
+      <router-link to="/Update"> Update </router-link>
+      <router-link to="/Watchlist"> Watchlist </router-link>
+    </div>
+
   
   </div>
 </template>
