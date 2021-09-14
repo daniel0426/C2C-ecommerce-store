f4b851065dd754902b9e1809ed51163eb019555e
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-09-14 14:42:08 +1200

    Formatting of router file, added Notifications and Account Router view. Added props to Detail route.

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,67 +1,81 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import CreateAccount from '../views/CreateAccount.vue'
-import CreateListing from '../views/CreateListing.vue'
-import Detail from '../views/Detail.vue'
-import MyListings from '../views/MyListings.vue'
-import Update from '../views/Update.vue'
-import Watchlist from '../views/Watchlist.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import CreateAccount from "../views/CreateAccount.vue";
+import CreateListing from "../views/CreateListing.vue";
+import PostDetail from "../views/PostDetail.vue";
+import MyListings from "../views/MyListings.vue";
+import Update from "../views/Update.vue";
+import Watchlist from "../views/Watchlist.vue";
+import Notifications from "../views/Notifications.vue";
+import Account from "../views/Account.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/CreateAccount',
-    name: 'CreateAccount',
-    component: CreateAccount
+    path: "/CreateAccount",
+    name: "CreateAccount",
+    component: CreateAccount,
   },
   {
-    path: '/CreateListing',
-    name: 'CreateListing',
-    component: CreateListing
+    path: "/CreateListing",
+    name: "CreateListing",
+    component: CreateListing,
   },
   {
-    path: '/Detail',
-    name: 'Detail',
-    component: Detail
+    path: "/PostDetail/:postId",
+    name: "PostDetail",
+    component: PostDetail,
+    props: true,
   },
   {
-    path: '/MyListings',
-    name: 'MyListings',
-    component: MyListings
+    path: "/MyListings",
+    name: "MyListings",
+    component: MyListings,
   },
   {
-    path: '/update',
-    name: 'Update',
-    component: Update
+    path: "/update",
+    name: "Update",
+    component: Update,
   },
   {
-    path: '/watchlist',
-    name: 'Watchlist',
-    component: Watchlist
-  }
-]
+    path: "/watchlist",
+    name: "Watchlist",
+    component: Watchlist,
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    component: Notifications,
+  },
+  {
+    path: "/Account",
+    name: "Account",
+    component: Account,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
