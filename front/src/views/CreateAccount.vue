1fc8e3264914e29b766f43f9933e96e33538b1fe
TimboNZ76 <custompc2010@live.com>
2021-09-14 12:46:58 +1200

    Updated project after pull requests was sent to main branch and a copy pulled down to my local computer

--- front/src/views/CreateAccount.vue
+++ front/src/views/CreateAccount.vue
@@ -1,6 +1,6 @@
 <template>
- <span @Click="GoBack" class="go-back text-dark-purple bg-transperant font-epilogue sm:text-lg  lg:text-2xl text-extrabold sm:m-10 lg:m-10  bg-cream sm:px-5 lg:px-10"> Back to Home</span> <br>
-<div class="container lg:flex md:flex sm:flex-none bg-cream w-full h-auto font-epilogue">  
+ <span @Click="GoBack" class="go-back text-dark-purple bg-transperant font-epilogue sm:text-lg  lg:text-2xl text-extrabold sm:m-10 lg:m-10 sm:h-auto  bg-cream sm:px-5 lg:px-10"> Back to Home</span> <br>
+<div class="container lg:flex md:flex sm:flex-none bg-cream w-screen h-screen font-epilogue place-items-center">  
   <div class="logo-area space-y-1 text-dark-purple font-epilogue m-20 w-1/3 text-center text-sm bg-cream  sm:hidden md:block lg:block place-items-center">    
   <h1 class="main-heading sm:text-xl md:text-3xl lg:text-5xl text-dark-purple text-bold font-epilogue" > Thrift Me</h1>
    <img src="https://picsum.photos/id/1025/300/300" alt="" class="logo sm:ml-10 md:ml-1 lg:ml-1">   
@@ -9,15 +9,15 @@
     <p>and customise your experience</p>
   </div>
 
-  <div class="signup text-center xsm:m-0 md:m-5 lg:m-10 bg-cream xsm:w-full sm:w-full lg;w2/3 block"> 
+  <div class="signup text-center sm:m-5 md:m-5 lg:m-10 sm:mt-5 bg-cream xsm:w-full sm:w-full lg;w2/3 block"> 
  <h1 class="title text-dark-purple font-epilogue text-bold sm:text-xl md:text-2xl lg:text-3xl text-center ">Sign Up</h1><br>
-    <span @click="GoLogin" class="subtitle text-dark-purple font-epilogue  text-sm text-center">Already a ThriftMe user? Sign in here</span><br><br>
+    <span @click="GoLogin" class="subtitle text-dark-purple font-epilogue  text-xs text-center">Already a ThriftMe user? <b><u>Sign in here</u></b></span><br><br>
     <form class="signup-form text-dark-purple sm:space-y-1 md:space-y-1 lg:space-y-1 text-sm  bg-cream text-center place-items-center" name="signup-form">
       <input type="text" class="fname bg-white placeholder-purple-grey text-center opacity-60 rounded-xl border-2 border-purple-grey border-solid px-5 py-1" name="fname" v-model="fname" placeholder="FIRST NAME"  required="true"><br>
        <span class="error text-red-600 test-sm" v-if="msg.fname">{{msg.fname}}</span><br>
       <input  type="text" class="lname bg-white placeholder-purple-grey text-center opacity-60 rounded-xl border-2 border-purple-grey border-solid px-5 py-1" name="lname" v-model="lname" placeholder="LAST NAME"  required="true"><br>
      <span class="error text-red-600 test-sm" v-if="msg.lname">{{msg.lname}}</span><br>
-      <input type="text" class="date-of-birth placeholder-purple-grey text-center opacity-60 rounded-xl border-2 border-purple-grey border-solid px-5 py-1" name="birth-date" v-model="dateofbirth"  placeholder="DD / MM / YYYY"  required="true"><br>
+      <input type="text" class="date-of-birth bg-white placeholder-purple-grey text-center opacity-60 rounded-xl border-2 border-purple-grey border-solid px-5 py-1" name="birth-date" v-model="dateofbirth"  placeholder="DD / MM / YYYY"  required="true"><br>
       <span class="error text-red-600 test-sm" v-if="msg.dateofbirth">{{msg.dateofbirth}}</span><br>
       <input type="email" class="email bg-white text-purple-grey placeholder-purple-grey text-center opacity-60 rounded-xl border-2 border-purple-grey border-solid px-5 py-1" name="email" v-model="email" placeholder="EMAIL  ADDRESS"  required="true"><br>
         <span class="error text-red-600 test-sm" v-if="msg.email">{{msg.email}}</span><br>
@@ -30,7 +30,7 @@
     <button class="signup-form-button bg-dark-purple rounded-xl text-sm px-5 py-2 text-cream">Create Account</button><br>
     </div>    
 </div>
-<footer class="footer sm:h-0 md:h-10 lg:h-10 bg-bright-purple "></footer>
+<footer class="footer sm:h-8 md:h-10 lg:h-10 bg-bright-purple "></footer>
 </template>
 
 <script>
--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,27 +1,28 @@
 <template>
-<span @Click="GoBack" class="go-back text-dark-purple bg-transperant font-epilogue sm:text-lg  lg:text-2xl text-extrabold sm:m-10 lg:m-10  bg-cream sm:px-5 lg:px-10"> Back to Home</span> <br>
- <div class="container lg:flex md:flex sm:flex-none bg-cream w-screen h-screen font-epilogue mt-10">
-   <div class="logo-area space-y-1 text-dark-purple font-epilogue m-20 w-1/3 text-center text-sm bg-cream  sm:hidden md:block lg:block place-items-center">    
+<span @Click="GoBack" class="go-back text-dark-purple bg-transperant font-epilogue sm:text-sm  lg:text-2xl text-extrabold sm:m-5 md:m5 lg:m-10  bg-cream sm:px-5 lg:px-10"> Back to Home</span> <br>
+ <div class="container lg:flex md:flex sm:flex-none bg-cream w-screen h-screen font-epilogue  place-items-center">
+   <div class="logo-area space-y-1 text-dark-purple font-epilogue sm:m-10 lg:m-20 w-1/3 text-center text-sm bg-cream  sm:hidden md:block lg:block place-items-center">    
   <h1 class="main-heading sm:text-xl md:text-3xl lg:text-5xl text-dark-purple text-bold font-epilogue" > Thrift Me</h1>
-   <img src="https://picsum.photos/id/1025/300/300" alt="" class="logo sm:ml-10 md:ml-1 lg:ml-1">   
-   <span @click="GoSignup" class="signup-link">New to Thriftme sign up here</span><br>
+   <img src="https://picsum.photos/id/1025/300/300" alt="" class="logo sm:ml-10 md:ml-1 lg:ml-1"> 
+   <span @click="GoSignup" class="signup-link">New to Thriftme <b><u>Create Account Here</u></b></span><br>  
+   
     
     </div>
-<div class="login-area text-center xsm:m-0 md:m-5 lg:m-20 bg-cream xsm:w-full sm:w-full lg;w2/3 block text-dark-purple text-sm "> 
-<h1 class="main-heading sm:text-xl md:text-3xl lg:text-4xl text-dark-purple text-bold font-epilogue">Login</h1>
+<div class="login-area text-center  bg-cream sm-h-screen xsm:w-screen sm:w-screen lg;w2/3 block text-dark-purple text-sm space-y-5  sm:mt-5 sm:m-6 "> 
+<h1 class="main-heading sm:text-xl md:text-3xl lg:text-4xl text-dark-purple text-bold font-epilogue">Login</h1><br>
 <p>If you are an existing user, enter your login below</p>
 <p>to enjoy the full Thriftme experience</p>
-<form class="login-form text-center space-y-5 m-5" name="login">
+<form class="login-form text-center  space-y-5 m-5" name="login">
     <input class="login-email bg-white placeholder-purple-grey text-center opacity-60 rounded-xl border-2 border-purple-grey border-solid px-5 py-1" type="text" name="login-email" vmodel="email" placeholder="Enter your email"/><br>
     <input class="login-password bg-white placeholder-purple-grey text-center opacity-60 rounded-xl border-2 border-purple-grey border-solid px-5 py-1" type="password" name="login-password" vmodel="password" placeholder="Enter your password"/>
   </form>
   <span>Forgot your login details?</span><br><br>
 
-  <button class="login-button bg-dark-purple rounded-xl text-sm px-5 py-2 text-cream">Login</button>
+  <button class="login-button bg-dark-purple rounded-xl text-sm px-5 py-2 text-cream">Login</button><br><br>
+  <span @click="GoSignup" class="signup-link md:hidden lg:hidden">New to Thriftme <b><u>Create Account Here</u></b></span><br>
 </div>
-
  </div>
-
+<footer class="footer sm:h-8 md:h-10 lg:h-10 bg-bright-purple place-self-flexend "></footer>
 </template>
 
 <script>
