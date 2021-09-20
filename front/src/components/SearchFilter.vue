18ce59631709c71e80ad3c5d6fa60337106f131f
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-09-20 13:24:58 +1200

    Amended comment Schema, guest options added to nav. Templated filtering components.

---
+++ front/src/components/SearchFilter.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>This is the search filter</h2>
+</template>
+
+<script>
+export default {
+  name: "SearchFilter",
+};
+</script>
+
+<style>
+</style>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -3,12 +3,31 @@
   <div class="">
     <div class="flex flex-col">
       <router-link to="/CreateAccount">Create Account </router-link>
-      <router-link to="/CreateListing"> Create Listing </router-link>
       <router-link to="/Detail"> Detail </router-link>
       <router-link to="/Login"> Login </router-link>
-      <router-link to="/MyListings"> My Listings </router-link>
       <router-link to="/Update"> Update </router-link>
-      <router-link to="/Watchlist"> Watchlist </router-link>
+    </div>
+
+    <div v-if="true" class="my-10 font-epilogue space-y-5 text-dark-purple">
+      <h2 class="text-4xl font-bold">Welcome, User Name!</h2>
+      <h3 class="font-semibold text-2xl">What would you like to do today?</h3>
+    </div>
+
+    <div v-else class="my-10 font-epilogue space-y-5 text-dark-purple">
+      <h2 class="text-4xl font-bold">Welcome, Guest!</h2>
+      <h3 class="font-semibold text-2xl">
+        See our current listings below, or create/login to your ThriftMe Account
+        for the full user experience.
+      </h3>
+    </div>
+
+    <h2 class="font-semibold text-2xl text-dark-purple font-epilogue">
+      Browse:
+    </h2>
+
+    <div class="space-y-4 mb-4">
+      <SearchFilter />
+      <CategoryFilter />
     </div>
 
     <div
@@ -37,11 +56,15 @@
 <script>
 // @ is an alias to /src
 import SinglePost from "../components/SinglePost.vue";
+import SearchFilter from "../components/SearchFilter.vue";
+import CategoryFilter from "../components/CategoryFilter.vue";
 
 export default {
   name: "Home",
   components: {
     SinglePost,
+    SearchFilter,
+    CategoryFilter,
   },
   data() {
     return {
