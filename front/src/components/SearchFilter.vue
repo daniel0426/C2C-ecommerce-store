e1360343ceab4113519fdda8843f74147d36106f
Alexis Cousins <alexis.simmons1997@gmail.com>
2021-09-22 15:43:45 +1200

    Tweak to search text color

--- front/src/components/SearchFilter.vue
+++ front/src/components/SearchFilter.vue
@@ -1,27 +1,58 @@
 <template>
-<div class="flex items-center ">
-  <h1 class="text-1xl font-bold text-bright-purple font-epilogue">I am looking for ...</h1>
-<form action="" @submit.prevent="filterByInput" class="flex flex-1 items-center md:w-1/2 m-auto mx-12 relative my-4 w-full">
-  <input v-model="item" class=" w-full px-4 py-2 border  border-bright-purple rounded-full placeholder-white text-bright-purple outline-none" type="text" placeholder="Seach items...">
-  <button type="submit" class="absolute right-4"><i class="text-xl font-medium fas fa-search text-bright-purple  "></i></button>
-</form>
-</div>
+  <div class="flex items-center border border-red-500">
+    <h1 class="text-1xl font-bold text-dark-purple font-epilogue">
+      I am looking for ...
+    </h1>
+    <form
+      action=""
+      @submit.prevent="filterByInput"
+      class="
+        flex flex-1
+        items-center
+        md:w-1/2
+        m-auto
+        mx-12
+        relative
+        my-4
+        w-full
+      "
+    >
+      <input
+        v-model="item"
+        class="
+          w-full
+          px-4
+          py-2
+          border border-bright-purple
+          rounded-full
+          placeholder-white
+          text-bright-purple
+          outline-none
+        "
+        type="text"
+        placeholder="Seach items..."
+      />
+      <button type="submit" class="absolute right-4">
+        <i class="text-xl font-medium fas fa-search text-bright-purple"></i>
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: "SearchFilter",
-  data(){
-    return{
-      item:null,
-    }
+  data() {
+    return {
+      item: null,
+    };
   },
   methods: {
-    filterByInput (e){
-      this.$emit("filteredByInput", this.item)
-      this.item=""
-    }
-  }
+    filterByInput(e) {
+      this.$emit("filteredByInput", this.item);
+      this.item = "";
+    },
+  },
 };
 </script>
 
